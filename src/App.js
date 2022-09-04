873f4d1fa4aa95b9a5a5421c2d408a53b7b7ff7d
Muhammed Ajmal <ajmalktl313@gmail.com>
2022-09-04 16:31:32 +0530

    firs upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+// import axios from "axios";
+import NavBar from "./Components/NavBar/NavBar";
+import './App.css'
+import Banner from "./Components/Banner/Banner";
+import RowPost from "./Components/RowPost/RowPost";
+import {originals,action,ComedyMovies,HorrorMovies,Documentaries} from './urls'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Banner/>
+      <RowPost url={originals}  title='Netflix Originals'/>
+      <RowPost url={action} title="Action" isSmall/>
+      <RowPost url={ComedyMovies} title="Comedy Movies" isSmall/>
+      <RowPost url={HorrorMovies} title="Horror Movies" isSmall/>
+      <RowPost url={Documentaries} title="Documentaries" isSmall/>
     </div>
   );
 }
